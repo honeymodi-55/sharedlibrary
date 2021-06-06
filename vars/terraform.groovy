326175159f461ad83632384eebe8d3e0fb005528
Mahidhar Reddy <mahidhar@outlook.com>
2021-06-06 12:28:32 -0400

    add shared library

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,9 +1,4 @@
-def call(Map config = [:], body){
-
-    def pipelineParams= [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = pipelineParams
-    body()
+def call(String access_key, String secret_key){
 
     pipeline {
     agent any 
@@ -18,7 +13,7 @@ def call(Map config = [:], body){
                 ]]){
                     sh 'terraform init -force-copy'
                     sh 'echo ${config.access_key}'
-                    sh 'terraform plan -var ${config.access_key}=${AWS_ACCESS_KEY_ID} -var ${config.secret_key}=${AWS_SECRET_ACCESS_KEY} -out Outputforplan'
+                    sh 'terraform plan -var ${access_key}=${AWS_ACCESS_KEY_ID} -var ${secret_key}=${AWS_SECRET_ACCESS_KEY} -out Outputforplan'
                     sh 'terraform apply -input=false Outputforplan'
                 }
                 }
