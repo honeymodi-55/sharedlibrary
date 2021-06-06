c65561903191554dff112c6825b8c15e93f5255d
Mahidhar Reddy <mahidhar@outlook.com>
2021-06-06 12:30:00 -0400

    add shared library

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -12,7 +12,6 @@ def call(String access_key, String secret_key){
                     secretKeyVariable: "AWS_SECRET_ACCESS_KEY"
                 ]]){
                     sh 'terraform init -force-copy'
-                    sh 'echo ${config.access_key}'
                     sh 'terraform plan -var ${access_key}=${AWS_ACCESS_KEY_ID} -var ${secret_key}=${AWS_SECRET_ACCESS_KEY} -out Outputforplan'
                     sh 'terraform apply -input=false Outputforplan'
                 }
