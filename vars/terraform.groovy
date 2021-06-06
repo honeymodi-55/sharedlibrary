a315e0df6e82425e2163942dd7f6614fad1164bf
Mahidhar Reddy <mahidhar@outlook.com>
2021-06-06 14:17:56 -0400

    add shared library

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,19 +1,18 @@
-def call(String access_key,String secret_key){
+def call(Map config = [:]){
 
     pipeline {
     agent any 
     stages {
-        stage ('deploy terraform') {
+        stage ("deploy terraform") {
             steps { 
                 withCredentials([[
                     $class: 'AmazonWebServicesCredentialsBinding',
                     credentialsId: "jenkins-terraform-user",
-                    accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                    accessKeyVariable: "AWS_ACCESS_KEY_ID",
                     secretKeyVariable: "AWS_SECRET_ACCESS_KEY"
                 ]]){
-                    sh "echo ${access_key}"
                     sh "terraform init -force-copy"
-                    sh "terraform plan -var ${access_key}=${AWS_ACCESS_KEY_ID} -var ${secret_key}=${AWS_SECRET_ACCESS_KEY} -out Outputforplan"
+                    sh "terraform plan -var ${config.access_key}=${AWS_ACCESS_KEY_ID} -var ${config.secret_key}=${AWS_SECRET_ACCESS_KEY} -out Outputforplan"
                     sh "terraform apply -input=false Outputforplan"
                 }
                 }
